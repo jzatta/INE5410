7ff3c315c271b207bd60f261bb04563be732b926
Johnaltan Neves <johnaltan@gmail.com>
2015-04-21 16:14:21 -0300

    Versao funcional do trabalho

--- Trab1.c
+++ Trab1.c
@@ -3,10 +3,11 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <malloc.h>
+#include <time.h>
 
-#define VECT_SIZE   100
+#define VECT_SIZE   100000
 #define BUCKETS     5
-#define THREADS     10
+#define THREADS     5 
 
 typedef struct {
     int size, minValue, maxValue;
@@ -24,26 +25,41 @@ void *sortIt(void *);
 int main(int argc, char **argv) {
     pthread_t *myThreads;
     int i, *vector;
-    
+
     vector = (int *)malloc(sizeof(int) * VECT_SIZE);
-    
-    srand(666);
+
+    srand(time(NULL));
     for (i = 0; i < VECT_SIZE; i++) {
         vector[i] = rand() % VECT_SIZE;
-        printf("%d, ", vector[i]);
+        printf("%d ", vector[i]);
     }
-    
+    printf("\n");
+
     arrayBuckets = (bucket_t *)calloc(sizeof(bucket_t), BUCKETS);
-    int quantMod = VECT_SIZE % BUCKETS - 1;
+    int quantMod = VECT_SIZE % BUCKETS;
     int quantDiv = VECT_SIZE / BUCKETS;
     arrayBuckets[0].minValue = 0;
-    arrayBuckets[0].maxValue = quantDiv + (quantMod-- > 0)?1:0;
-    arrayBuckets[0].bucket = (int *)malloc(sizeof(int) * VECT_SIZE);
+    arrayBuckets[0].maxValue = quantDiv + ((quantMod-- > 0)?1:0) - 1;
     for (i = 1; i < BUCKETS; i++) {
         arrayBuckets[i].minValue = arrayBuckets[i - 1].maxValue + 1;
-        arrayBuckets[i].maxValue = arrayBuckets[i].minValue + quantDiv + (quantMod-- > 0)?1:0;
+        arrayBuckets[i].maxValue = arrayBuckets[i].minValue + quantDiv + ((quantMod-- > 0)?1:0) - 1;
     }
-    
+
+    for (i = 0; i < VECT_SIZE; i++) {
+        int j;
+        for (j = 0; j < BUCKETS; j++) {
+            if ((vector[i] >= arrayBuckets[j].minValue) && (vector[i] <= arrayBuckets[j].maxValue)) {
+                arrayBuckets[j].size++;
+                break;
+            }
+        }
+    }
+
+    for (i = 0; i < BUCKETS; i++) {
+        arrayBuckets[i].bucket = (int *)malloc(sizeof(int) * arrayBuckets[i].size);
+        arrayBuckets[i].size = 0;
+    }
+
     for (i = 0; i < VECT_SIZE; i++) {
         int j;
         for (j = 0; j < BUCKETS; j++) {
@@ -53,20 +69,35 @@ int main(int argc, char **argv) {
             }
         }
     }
-    
-    //realloc arrayBuckets.bucket and delete empty buckets
-    
-    
+
+    for (i = 0; i < BUCKETS; i++) {
+        int j;
+        printf("Bucket %d (min= %d, max=%d): ",i, arrayBuckets[i].minValue, arrayBuckets[i].maxValue);
+        for (j = 0; j < arrayBuckets[i].size; j++)
+            printf("%d ",arrayBuckets[i].bucket[j]);
+        printf("\n");
+    }
+    printf("\n");
+
+    usedBucket = 0;
     pthread_mutex_init(&usedBucketMutex, NULL);
     myThreads = (pthread_t *)malloc(sizeof(pthread_t) * THREADS);
-    for (i = 0; i < THREADS; i++)	
-        pthread_create(&myThreads[i], NULL, sortIt, NULL);
+    for (i = 0; i < THREADS; i++)
+        pthread_create(&myThreads[i], NULL, sortIt, (void *)i);
     for (i = 0; i < THREADS; i++) {
         pthread_join(myThreads[i],NULL);
     }
+
+    for (i = 0; i < BUCKETS; i++) {
+        int j;
+        for (j = 0; j < arrayBuckets[i].size; j++)
+            printf("%d ",arrayBuckets[i].bucket[j]);
+    }
+    printf("\n");
+
     free(myThreads);
     pthread_mutex_destroy(&usedBucketMutex);
-    
+
     for (i = 0; i < BUCKETS; i++) {
         free(arrayBuckets[i].bucket);
     }
@@ -75,8 +106,9 @@ int main(int argc, char **argv) {
 }
 
 void *sortIt(void *arg) {
+    int threadIndex = (int)arg;
     int bucketIndex;
-    while ("Todo o sempre!") {
+    while ("The sun still hot!") {
         pthread_mutex_lock(&usedBucketMutex);
         bucketIndex = usedBucket++;
         pthread_mutex_unlock(&usedBucketMutex);
@@ -86,6 +118,7 @@ void *sortIt(void *arg) {
         else if (arrayBuckets[bucketIndex].size == 0) {
             continue;
         }
+        printf("Thread %d processando bucket %d\n", threadIndex, bucketIndex);
         bubbleSort(arrayBuckets[bucketIndex].bucket, arrayBuckets[bucketIndex].size);
     }
     return NULL;
@@ -101,3 +134,4 @@ void bubbleSort(int *buf, int size) {
                 buf[j+1] = aux;
             }
 }
+
